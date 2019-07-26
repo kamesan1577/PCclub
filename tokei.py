d5f476514de8ba74115cb7ad084db353ac91481f
kamesan <47214420+kamesan1577@users.noreply.github.com>
2019-07-26 11:55:48 +0900

    Add files via upload

--- tokei.py
+++ tokei.py
@@ -11,27 +11,20 @@ kakudo_fun = dt_now.minute
 kakudo_zikan = dt_now.hour
 #背景の円を描画
 speed(1000)
-circleback = penup()
-circleback = setpos(0,-325)
-circleback = pendown()
-circleback = circle(325)
+
 #時計の針を描画
 byo = Turtle()
 byo.width(5)
-byo.rt(kakudo_byo*6-90)　#一秒に動く針の角度
-byo.fd(320)
+#一秒に動く針の角度
+
 fun = Turtle()
 fun.width(10)
-fun.rt(kakudo_fun*6-90)
-fun.fd(285)
+
 zikan = Turtle()
 zikan.width(15)
-zikan.rt(kakudo_zikan*30-90)
-zikan.fd(200)
+
 #仮のコード（よくわからない）
-hari = [byo,fun,zikan]
-hari = color("white")
-timecount = Turtle()
+
 circleback = Turtle()
 i = 0
 #プロンプトに時刻を表示
@@ -42,5 +35,19 @@ print(kakudo_byo)
 hideturtle()
 begin_fill()
 time_sta = time.perf_counter()
+circleback = penup()    
+circleback = setpos(0,-325)
+circleback = pendown()
+circleback = circle(325)
+while i < 1:
+    byo = setpos(0,0)
+    byo.rt(kakudo_byo*6-90)
+    byo.fd(320)
+    fun.rt(kakudo_fun*6-90)
+    fun.fd(285)
+    zikan.rt(kakudo_zikan*30-90)
+    zikan.fd(200)
+    byo.clear()
+    
 
 input("prompt: ")
